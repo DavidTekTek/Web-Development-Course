493ee614b7e1be4752b272e16d78522c69285e01
DavidTekTek <iolamilekann@gmail.com>
2025-11-11 22:03:27 +0100

    new files

--- play.py
+++ play.py
@@ -1,29 +1,17 @@
-# 1. Import Tkinter Module
-# 2. Create the GUI application main Window
-# 3. Add widgets
+import random
 
-from tkinter import *
-
-window = Tk()
-window.title('Tkinter Sample Window')
-window.geometry('300x300')
-
-# Label
-greeting = Label(text="Hello User", fg='black', bg='white')
-# Button 
-button = Button(text="Click me", bg='black', fg='white')
-# Entry 
-entry = Entry(fg="yellow", bg="blue", width=50)
+import time
 
 
-greeting.pack()
-button.pack()
-entry.pack()
+def getRandomDate(starDate, endDate):
+    print("Printing random date between", starDate, "and", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%m/%d/%Y'
+    starttime = time.mktime(time.strptime(starDate, dateFormat))
+    endtime = time.mktime(time.strptime(endDate, dateFormat))
+    randomTime = starttime + randomGenerator * (endtime - starttime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
 
-frame = Frame(master=window, relief=RAISED, borderwidth=5)
-frame.pack()
-label = Label(master=frame, text='Sample Frame')
-label.pack()
 
-textbox = Text(fg='green', bg='yellow')
-textbox.pack()
+print("Random Date = ", getRandomDate("1/1/2020", "12/12/2024"))
